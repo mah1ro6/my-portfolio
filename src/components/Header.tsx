801a8c7476b5aaaf8775de8795f320704de84525
mahiro <mahiro@mahiro.local>
2021-09-25 01:10:45 +0900

    ESlint,prettierの設定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
-  return(
-    <div>
-      header
-    </div>
-  )
-}
+  return <div>header</div>;
+};
 
 export default Header;
