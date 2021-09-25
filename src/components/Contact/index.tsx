dba4f06468d5f14b306a5fe820fff7e0d741c1e0
mahiro <mahiro@mahiro.local>
2021-09-25 18:51:03 +0900

    tailwindcssの導入

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+// import 'tailwindcss/tailwind.css';
 
 const Contact: React.FC = () => {
-  return <div>hello,Contact</div>;
+  return (
+    <div>
+      <h1 className="font-bold">hello,Contact</h1>
+    </div>
+  );
 };
 
 export default Contact;
